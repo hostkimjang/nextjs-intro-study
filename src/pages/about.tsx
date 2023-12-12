501e05cc8acd796e1c432668d696b4a00bbe3024
hostkimjang <kyskys2284@naver.com>
2023-12-12 09:41:57 +0900

    Init

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/NavBar";
+import Seo from "../../components/Seo";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+      <p>This is the about us page</p>
+    </div>
+  );
+}
